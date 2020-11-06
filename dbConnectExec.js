121a1712cc2f9c08a66a560f1467682ea98d8ecc
krmcvay <krmcvay@rams.colostate.edum>
2020-11-05 22:28:53 -0700

    10.1 11/5 databse, /books

---
+++ dbConnectExec.js
@@ -0,0 +1,23 @@
+const sql = require('mssql')
+const mcvayConfig = required('./config.js')
+
+const config = {
+    user: mcvayConfig.DB.user,
+    password: mcvayConfig.DB.password,
+    server: mcvayConfig.DB.server, 
+    database: mcvayConfig.DB.database,
+}
+
+async function executeQuery(aQuery){
+    var connection = await sql.connect(config)
+    var result = await connection.query(aQuery)
+       
+    return result.recordset
+    // console.log(result)   
+}
+
+module.exports = {executeQuery: executeQuery}
+// executeQuery(`SELECT *
+//     FROM book
+//     LEFT JOIN genre
+//     ON genre.GenrePK = book.GenreFK`)
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const db = require('./dbConnectExec.js')
 
 const app = express();
 
@@ -6,6 +7,22 @@ app.get("/hi", (req,res)=>{
     res.send("hello world")
 })
 
+app.get("/books", (req,res)=>{
+    //get data from database
+    db.executeQuery(`SELECT *
+    FROM book
+    LEFT JOIN genre
+    ON genre.GenrePK = book.GenreFK`)
+    .then((result)=>{
+        res.status(200).send(result)
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.status(500).send()
+    })
+})
+
 app.listen(5000,()=>{
     console.log("app is running on port 5000")
 })
+
